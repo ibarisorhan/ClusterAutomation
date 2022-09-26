5cca2f63a776b2f3e52ad1ea112256628ca3d5bc
ibarisorhan <87118086+ibarisorhan@users.noreply.github.com>
2022-09-27 01:25:27 +1000

    Update GatherErrored.sh

--- Other/GatherErrored.sh
+++ Other/GatherErrored.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep "WARNING: INAPPROPRIATE NUMBER OF UNIT CELLS USED" ./System_0/*.data >> Errored.txt
+grep -m 1 "WARNING: INAPPROPRIATE NUMBER OF UNIT CELLS USED" ./System_0/*.data >> Errored.txt
