b58114fb083f65ec180967977f737b3dcee55f40
ibarisorhan <87118086+ibarisorhan@users.noreply.github.com>
2022-09-27 15:20:58 +1000

    Update parseTasks.py

--- RASPA_automation/Extras/parseTasks.py
+++ RASPA_automation/Extras/parseTasks.py
@@ -2,4 +2,4 @@ f = open('tasks.txt','r')
 data = f.read()
 f.close()
 
-print(len(data.split('.gadi-pbs')))
+print(len(data.split('.gadi-pbs'))) #ASSUMES THIS SPECIFIC CLUSTER + NAMING CONVENTION IS USED
