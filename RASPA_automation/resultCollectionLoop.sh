a05aaa0f27040b56e0c129bca8b314fc9c94804f
ibarisorhan <87118086+ibarisorhan@users.noreply.github.com>
2022-11-02 11:53:52 +1100

    Update resultCollectionLoop.sh

--- RASPA_automation/resultCollectionLoop.sh
+++ RASPA_automation/resultCollectionLoop.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 current=$(pwd)
-python3 Tools/setHeader.py ../$1
+sim_type=$(python3 ./Tools/returnSimType.py)
+doc_name="${sim_type}_output.csv"
+python3 Tools/setOutputHeader.py ../${doc_name}
 cd ../MonteCarlo
 
 for mof_dir in */; do
 cd $current
-cd ../MonteCarlo/Output/System_0
+cd ../$sim_type/Output/System_0
 #cd ./$mof_dir/Output/System_0
 
 for result in *.data; do
 echo "$(pwd)"
 echo $result
-python3 ${current}/Tools/pythonResultCollectionTool.py $result ../../../$1
+python3 ${current}/Tools/pythonResultCollectionTool.py $result ../../../${doc_name}
 done
-cd ${current}/../MonteCarlo
+cd ${current}/../$sim_type
 done
-
