25498ea3daa0c4cec49ab9dc335aea3e81e74c69
ibarisorhan <87118086+ibarisorhan@users.noreply.github.com>
2024-10-22 00:13:50 +1100

    Update resultCollectionLoop.sh

--- RASPA_automation/resultCollectionLoop.sh
+++ RASPA_automation/resultCollectionLoop.sh
@@ -7,13 +7,18 @@ cd ../MonteCarlo
 
 for mof_dir in */; do
 cd $current
+echo "document,MOF,Unitcell,Temperature,Pressure,uptake,errorMargin,hasError,simulationComplete\n">>../$doc_name
 cd ../$sim_type/Output/System_0
 #cd ./$mof_dir/Output/System_0
 
 for result in *.data; do
 echo "$(pwd)"
 echo $result
-python3 ${current}/Tools/pythonResultCollectionTool.py $result ../../../${doc_name}
+if grep -q "Simulation finished" $result; then
+python3 ${current}/Tools/pythonResultCollectionTool.py $result ../../../${doc_name};
+else
+python3 ${current}/Tools/midSiumulationResultCollectionTool.py $result ../../../${doc_name};
+fi
 done
 cd ${current}/../$sim_type
 done
